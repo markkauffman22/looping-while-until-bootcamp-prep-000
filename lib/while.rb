b84f9037f721b09e10096eed41566d63642009cf
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 16:58:49 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,12 +3,13 @@
 def using_while () 
   levitation_force = 7
     while levitation_force <= 10
+      levitation_force += 1
       if levitation_force == 10
         puts "Wingardium Leviosa"
         break
       else  
         puts "Wingardium Leviosa"
-        levitation_force += 1
+
     end
   end  
 end
