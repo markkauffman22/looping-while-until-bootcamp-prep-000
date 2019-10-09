66f83c15e37f58e6318dc87d423e81c1046cac5f
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 16:33:31 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,20 +1,4 @@
 def using_until
   puts "HOLDER"
 end   
- levitation_force = 6 
-  #your code here
-def using_while (levitation_force) 
-  if levitation_force = 10
-    puts "Wingardium Leviosa"
-    break
-  else  
-    while counter < 10
-      print "Levitation Force is "
-      puts levitation_force
-      levitation_force += 1
-    end
-  end  
-end
-
-using_while (levitation_force) 
 
