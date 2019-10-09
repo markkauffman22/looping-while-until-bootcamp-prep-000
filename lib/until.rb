3bc99a677864d7b3d4ca223f743007948992bcc8
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 16:29:16 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,11 +2,12 @@ def using_until
  levitation_force = 6 
   #your code here
 def using_while (levitation_force) 
+  if 
   while counter < 10
     print "Levitation Force is "
     puts levitation_force
     levitation_force += 1
-end
+  end
 end
     
 end
